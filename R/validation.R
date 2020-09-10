e2f5d1ba79e87686b3f63acca3d63983fc0a8433
Maria Doyle <maria.doyle@petermac.org>
2020-09-10 20:44:29 +1000

    Fix BiocCheck error package should be imported in the NAMESPACE file
    Made edits here to fix this:
    * Checking if other packages can import this one...
        * ERROR: Packages providing 2 object(s) used in this
          package should be imported in the NAMESPACE file,
          otherwise packages importing this package may
          fail.
    
          package::object in function()
            dplyr::n in is_not_duplicated()
            dplyr::n in is_rectangular()

--- R/validation.R
+++ R/validation.R
@@ -4,14 +4,14 @@ is_rectangular <- function(.data){
   is_rectangular_sample =
     .data %>%
     count(sample) %>%
-    count(`n`, name = "nn") %>%
+    count(dplyr::n, name = "nn") %>%
     nrow %>%
     equals(1)
 
   is_rectangular_transcript =
     .data %>%
     count(transcript) %>%
-    count(`n`, name = "nn") %>%
+    count(dplyr::n, name = "nn") %>%
     nrow %>%
     equals(1)
 
@@ -24,7 +24,7 @@ is_not_duplicated <- function(.data){
 
   .data %>%
     count(`sample`, `transcript`) %>%
-    filter(`n` > 1) %>%
+    filter(dplyr::n > 1) %>%
     nrow %>%
     equals(0)
 }
