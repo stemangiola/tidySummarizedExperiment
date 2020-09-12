8ee18a671d660a41904b9697b2c1f0f9a2151a95
Maria Doyle <maria.doyle@petermac.org>
2020-09-12 14:01:08 +1000

    Change ggplot and plotly tbl_df not default
    To fix the error about C stack usage

--- R/plotly_methods.R
+++ R/plotly_methods.R
@@ -146,7 +146,7 @@ plot_ly <- function(data=data.frame(), ..., type=NULL, name=NULL,
 
 #' @export
 #'
-plot_ly.default <- function(data=data.frame(), ..., type=NULL, name=NULL,
+plot_ly.tbl_df <- function(data=data.frame(), ..., type=NULL, name=NULL,
     color=NULL, colors=NULL, alpha=NULL,
     stroke=NULL, strokes=NULL, alpha_stroke=1,
     size=NULL, sizes=c(10, 100),
