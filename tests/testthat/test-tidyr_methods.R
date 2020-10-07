4d978a087d6fa8a4cda79cec3b51d920fcfe472f
stemangiola <mangiolastefano@gmail.com>
2020-10-07 16:08:35 +1100

    tests code style

--- tests/testthat/test-tidyr_methods.R
+++ tests/testthat/test-tidyr_methods.R
@@ -13,8 +13,13 @@ test_that("nest_unnest", {
 
     y <- tibble::tibble(
         sample = c(
-            "untrt1", "untrt2", "untrt3", "untrt4",
-            "trt1", "trt2", "trt3"
+            "untrt1",
+            "untrt2",
+            "untrt3",
+            "untrt4",
+            "trt1",
+            "trt2",
+            "trt3"
         ),
         counts = c(0L, 0L, 0L, 0L, 0L, 0L, 1L)
     )
@@ -32,23 +37,43 @@ test_that("nest_unnest", {
 test_that("unite separate", {
     un <- tt %>% unite("new_col", c(condition, col2), sep = ":")
 
-    expect_equal(un %>% tidySE::select(new_col) %>% slice(1) %>% pull(new_col), "untreated:other_col")
+    un %>%
+        tidySE::select(new_col) %>%
+        slice(1) %>%
+        pull(new_col) %>%
+        expect_equal("untreated:other_col")
 
-    se <- un %>% separate(col = new_col, into = c("orig.ident", "condition"), sep = ":")
+    se <-
+        un %>%
+        separate(
+            col = new_col,
+            into = c("orig.ident", "condition"),
+            sep = ":"
+        )
 
-    expect_equal(se %>% tidySE::select(sample) %>% ncol(), 1)
+    se %>%
+        tidySE::select(sample) %>%
+        ncol() %>%
+        expect_equal(1)
 })
 
 test_that("extract", {
-    expect_equal(
-        tt %>% tidySE::extract(col2, into = "g", regex = "other_([a-z]+)", convert = TRUE) %>% tidySE::pull(g) %>% class(),
-        "character"
-    )
+    tt %>%
+        tidySE::extract(col2,
+                        into = "g",
+                        regex = "other_([a-z]+)",
+                        convert = TRUE) %>%
+        tidySE::pull(g) %>%
+        class() %>%
+        expect_equal("character")
 })
 
 test_that("pivot_longer", {
-    expect_equal(
-        tt %>% tidySE::pivot_longer(c(sample, condition), names_to = "name", values_to = "value") %>% class() %>% .[1],
-        "tbl_df"
-    )
+    tt %>%
+        tidySE::pivot_longer(c(sample, condition),
+                             names_to = "name",
+                             values_to = "value") %>%
+        class() %>%
+        .[1] %>%
+        expect_equal("tbl_df")
 })
