0ec9a8c42bc10ddfed9558535dd7296f32f30bda
stemangiola <mangiolastefano@gmail.com>
2020-10-11 12:24:08 +1100

    add pasilla documentation

--- R/data.R
+++ R/data.R
@@ -1,4 +1,9 @@
-#' Example data set
+#' Read counts of RNA-seq samples of Pasilla knock-down by Brooks et al.
 #'
+#' A SummarizedExperiment dataset containing the transcriptome information for Drosophila Melanogaster.
 #'
+#' @format  containing 14599 features and 7 biological replicates.
+#'
+#' @source \url{https://bioconductor.org/packages/release/data/experiment/html/pasilla.html}
+#' @usage data(pasilla)
 "pasilla"
