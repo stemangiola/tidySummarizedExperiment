2ef286aa03d2f4234364d187a140ca98854871db
stemangiola <mangiolastefano@gmail.com>
2021-04-09 13:44:01 +1000

    test felix examples

---
+++ tests/testthat/test-felix.R
@@ -0,0 +1,62 @@
+context("felix test")
+
+library(magrittr)
+library(tidySummarizedExperiment)
+
+# Create dataset
+nrows <- 200; ncols <- 6
+counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
+rowRanges <- GRanges(rep(c("chr1", "chr2"), c(50, 150)),
+                     IRanges(floor(runif(200, 1e5, 1e6)), width=100),
+                     strand=sample(c("+", "-"), 200, TRUE),
+                     feature_id=sprintf("ID%03d", 1:200))
+colData <- DataFrame(Treatment=rep(c("ChIP", "Input"), 3),
+                     row.names=LETTERS[1:6])
+rse <- SummarizedExperiment(assays=SimpleList(counts=counts),
+                            rowRanges=rowRanges, colData=colData)
+
+
+
+test_that("Example 1 all columns included", {
+    
+    rse  %>% 
+        as_tibble() %>%
+        nrow() %>%
+        expect_equal(1200)
+})
+
+
+test_that("Example 2", {
+    
+    colData(rse)$sample <- seq_len(ncol(rse))
+    rowData(rse)$transcript <- seq_len(nrow(rse))
+    rse  %>% 
+        as_tibble() %>%
+        nrow() %>%
+        expect_equal(1200)
+})
+
+test_that("Example 3", {
+    
+    rowRanges(rse) <- split(rowRanges(rse),seq_len(nrow(rse)))
+    
+    rse  %>% 
+        as_tibble() %>%
+        nrow() %>%
+        expect_equal(1200)
+    
+    rowData(rse)$transcript <- seq_len(nrow(rse))
+    
+    rse  %>% 
+        as_tibble() %>%
+        nrow() %>%
+        expect_equal(1200)
+    
+    colnames(rse) <- NULL
+    
+    rse  %>% 
+        as_tibble() %>%
+        select(sample, feature) %>%
+        ncol() %>%
+        expect_equal(2) 
+})
