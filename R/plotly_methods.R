5b508eb14c32fd412e763366f35896889c42b286
William Hutchison <william.8191@fastmail.com>
2023-10-05 15:46:19 +1100

    Update plot_ly example

--- R/plotly_methods.R
+++ R/plotly_methods.R
@@ -5,78 +5,80 @@
 #' 
 #' @examples
 #' data(pasilla)
-#' plot_ly(pasilla)
+#' pasilla |>
+#'     plot_ly(x = ~ counts)
 #' 
 #' @importFrom plotly plot_ly
 #' @export
 setGeneric("plot_ly", 
     function(data=data.frame(), ..., type=NULL, name=NULL,
-        color=NULL, colors=NULL, alpha=NULL,
-        stroke=NULL, strokes=NULL, alpha_stroke=1,
-        size=NULL, sizes=c(10, 100),
-        span=NULL, spans=c(1, 20),
-        symbol=NULL, symbols=NULL,
-        linetype=NULL, linetypes=NULL,
-        split=NULL, frame=NULL,
-        width=NULL, height=NULL, source="A") {
-            standardGeneric("plot_ly")
+             color=NULL, colors=NULL, alpha=NULL,
+             stroke=NULL, strokes=NULL, alpha_stroke=1,
+             size=NULL, sizes=c(10, 100),
+             span=NULL, spans=c(1, 20),
+             symbol=NULL, symbols=NULL,
+             linetype=NULL, linetypes=NULL,
+             split=NULL, frame=NULL,
+             width=NULL, height=NULL, source="A") {
+      
+        standardGeneric("plot_ly")
 })
 
 #' @rdname plot_ly
 #' @export
 setMethod("plot_ly", "tbl_df",
     function(data=data.frame(), ..., type=NULL, name=NULL,
-    color=NULL, colors=NULL, alpha=NULL,
-    stroke=NULL, strokes=NULL, alpha_stroke=1,
-    size=NULL, sizes=c(10, 100),
-    span=NULL, spans=c(1, 20),
-    symbol=NULL, symbols=NULL,
-    linetype=NULL, linetypes=NULL,
-    split=NULL, frame=NULL,
-    width=NULL, height=NULL, source="A") {
-    data %>%
+             color=NULL, colors=NULL, alpha=NULL,
+             stroke=NULL, strokes=NULL, alpha_stroke=1,
+             size=NULL, sizes=c(10, 100),
+             span=NULL, spans=c(1, 20),
+             symbol=NULL, symbols=NULL,
+             linetype=NULL, linetypes=NULL,
+             split=NULL, frame=NULL,
+             width=NULL, height=NULL, source="A") {
+      
+        data %>%
 
-        # This is a trick to not loop the call
-        drop_class("tbl_df") %>%
-        plotly::plot_ly(...,
-            type=type, name=name,
-            color=color, colors=colors, alpha=alpha,
-            stroke=stroke, strokes=strokes, alpha_stroke=alpha_stroke,
-            size=size, sizes=sizes,
-            span=span, spans=spans,
-            symbol=symbol, symbols=symbols,
-            linetype=linetype, linetypes=linetypes,
-            split=split, frame=frame,
-            width=width, height=height, source=source
-        )
+            # This is a trick to not loop the call
+            drop_class("tbl_df") %>%
+            plotly::plot_ly(..., type=type, name=name,
+                            color=color, colors=colors, alpha=alpha,
+                            stroke=stroke, strokes=strokes, alpha_stroke=alpha_stroke,
+                            size=size, sizes=sizes,
+                            span=span, spans=spans,
+                            symbol=symbol, symbols=symbols,
+                            linetype=linetype, linetypes=linetypes,
+                            split=split, frame=frame,
+                            width=width, height=height, source=source
+                            )
 })
 
 #' @rdname plot_ly
 #' @export
 setMethod("plot_ly", "SummarizedExperiment",
-    function(data=data.frame(),
-    ..., type=NULL, name=NULL,
-    color=NULL, colors=NULL, alpha=NULL,
-    stroke=NULL, strokes=NULL, alpha_stroke=1,
-    size=NULL, sizes=c(10, 100),
-    span=NULL, spans=c(1, 20),
-    symbol=NULL, symbols=NULL,
-    linetype=NULL, linetypes=NULL,
-    split=NULL, frame=NULL,
-    width=NULL, height=NULL, source="A") {
+    function(data=data.frame(), ..., type=NULL, name=NULL,
+             color=NULL, colors=NULL, alpha=NULL,
+             stroke=NULL, strokes=NULL, alpha_stroke=1,
+             size=NULL, sizes=c(10, 100),
+             span=NULL, spans=c(1, 20),
+             symbol=NULL, symbols=NULL,
+             linetype=NULL, linetypes=NULL,
+             split=NULL, frame=NULL,
+             width=NULL, height=NULL, source="A") {
+
     data %>%
 
         # This is a trick to not loop the call
         as_tibble() %>%
         plotly::plot_ly(...,
-            type=type, name=name,
-            color=color, colors=colors, alpha=alpha,
-            stroke=stroke, strokes=strokes, alpha_stroke=alpha_stroke,
-            size=size, sizes=sizes,
-            span=span, spans=spans,
-            symbol=symbol, symbols=symbols,
-            linetype=linetype, linetypes=linetypes,
-            split=split, frame=frame,
-            width=width, height=height, source=source
-        )
+                        type=type, name=name,
+                        color=color, colors=colors, alpha=alpha,
+                        stroke=stroke, strokes=strokes, alpha_stroke=alpha_stroke,
+                        size=size, sizes=sizes,
+                        span=span, spans=spans,
+                        symbol=symbol, symbols=symbols,
+                        linetype=linetype, linetypes=linetypes,
+                        split=split, frame=frame,
+                        width=width, height=height, source=source
+                        )
 })
