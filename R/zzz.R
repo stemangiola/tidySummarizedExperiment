e69bc5c6509189d59c5c42618fafe5b7fcbc543b
Stefano Mangiola <mangiolastefano@gmail.com>
2025-07-10 12:04:48 +0930

    remove deprecated print method, and adjust NAMESPACE. Add startup message for external printing handling.

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,11 @@
 #' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname) {
     attached <- tidyverse_attach()
+    
+    # Print loading message about printing
+    packageStartupMessage(
+        "tidySummarizedExperiment says: Printing is now handled externally. ",
+        "If you want to visualize the data in a tidy way, do library(tidyprint). ",
+        "See https://github.com/tidyomics/tidyprint for more information."
+    )
 }
